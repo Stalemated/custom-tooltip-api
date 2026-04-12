2059f38e3c4495db6f96f08b92f10a7faedd30f5
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-04-12 01:01:28 -0600

    Feat: Added a button at the top right of the config screen to toggle the icon aligning feature

--- src/client/java/com/stalemated/customtooltips/gui/TooltipListScreen.java
+++ src/client/java/com/stalemated/customtooltips/gui/TooltipListScreen.java
@@ -1,8 +1,12 @@
 package com.stalemated.customtooltips.gui;
 
+import com.stalemated.customtooltips.ConfigManager;
 import com.stalemated.customtooltips.TooltipEntry;
+import com.stalemated.customtooltips.config.TooltipConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 
 import java.util.ArrayList;
@@ -22,6 +26,13 @@ public class TooltipListScreen extends Screen {
         this.listWidget = new TooltipListWidget(this.client, this.width, this.height, 32, this.height - 32, 25);
         this.addSelectableChild(this.listWidget);
 
+        this.addDrawableChild(ButtonWidget.builder(getAlignIconsText(), button -> {
+            TooltipConfig config = ConfigManager.getConfig();
+            config.align_attribute_icons = !config.align_attribute_icons;
+            AutoConfig.getConfigHolder(TooltipConfig.class).save();
+            button.setMessage(getAlignIconsText()); // Actualiza el texto (ON/OFF) dinámicamente
+        }).dimensions(this.width - 135, 6, 125, 20).build());
+
         this.addDrawableChild(ButtonWidget.builder(Text.translatable("customtooltips.tooltip_list_screen.add_new_tooltip"), button -> {
             TooltipEntry newEntry = new TooltipEntry();
             newEntry.colors = new ArrayList<>(Arrays.asList("gray"));
@@ -35,6 +46,14 @@ public class TooltipListScreen extends Screen {
         this.addDrawableChild(ButtonWidget.builder(Text.translatable("gui.done"), button -> {
             this.close();
         }).dimensions(this.width / 2 + 5, this.height - 28, 150, 20).build());
+    }
+
+    // Helper para generar el texto dinámico "Align Icons: ON" / "Align Icons: OFF"
+    private Text getAlignIconsText() {
+        boolean isOn = ConfigManager.getConfig().align_attribute_icons;
+        return Text.translatable("customtooltips.tooltip_list_screen.align_icons")
+                .append(": ")
+                .append(isOn ? ScreenTexts.ON : ScreenTexts.OFF);
     }
 
     @Override
