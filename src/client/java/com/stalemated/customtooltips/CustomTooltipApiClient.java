c32c93bfbb01a9a1ad33f60401fc00471665237c
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-04-20 23:05:41 -0600

    Feat: Implemented a keybind to open the config (unbound by default)

--- src/client/java/com/stalemated/customtooltips/CustomTooltipApiClient.java
+++ src/client/java/com/stalemated/customtooltips/CustomTooltipApiClient.java
@@ -3,15 +3,21 @@ package com.stalemated.customtooltips;
 import com.stalemated.customtooltips.core.IconAligner;
 import com.stalemated.customtooltips.core.TooltipProcessor;
 import com.stalemated.customtooltips.core.TooltipRegistry;
+import com.stalemated.customtooltips.gui.TooltipListScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,12 +26,26 @@ public class CustomTooltipApiClient implements ClientModInitializer {
 
 	public static final String MOD_ID = "customtooltips";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static KeyBinding openConfigKeybind;
 
 	@Override
 	public void onInitializeClient() {
 		ConfigManager.register();
 
 		TooltipRegistry.reload();
+
+		openConfigKeybind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.customtooltips.open_config",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_UNKNOWN,
+				"category.customtooltips.keys"
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (openConfigKeybind.wasPressed()) {
+				client.setScreen(new TooltipListScreen(client.currentScreen));
+			}
+		});
 
 		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
 			@Override
