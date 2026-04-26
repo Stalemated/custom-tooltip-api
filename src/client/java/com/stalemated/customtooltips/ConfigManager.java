bd711a971a0f52825b9b18c7867ffab3331c670a
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-04-25 19:26:18 -0600

    Feat: added back feature of regenerating the config if the config file is completely empty

--- src/client/java/com/stalemated/customtooltips/ConfigManager.java
+++ src/client/java/com/stalemated/customtooltips/ConfigManager.java
@@ -7,19 +7,39 @@ import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 
+import java.io.File;
+import java.nio.file.Path;
+
+import static com.stalemated.customtooltips.CustomTooltipApiClient.LOGGER;
+
 public class ConfigManager {
+
+    private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("custom_tooltip_api").resolve("config.json5");
 
     public static final ConfigClassHandler<TooltipConfig> HANDLER = ConfigClassHandler.createBuilder(TooltipConfig.class)
             .id(new Identifier("customtooltips", "config"))
             .serializer(config -> GsonConfigSerializerBuilder.create(config)
-                    .setPath(FabricLoader.getInstance().getConfigDir().resolve("custom_tooltip_api").resolve("config.json5"))
+                    .setPath(CONFIG_PATH)
                     .setJson5(true)
                     .build())
             .build();
 
     public static void register() {
+        File configFile = CONFIG_PATH.toFile();
+        boolean isNewOrEmpty = !configFile.exists() || configFile.length() == 0;
+
+        if (configFile.exists() && configFile.length() == 0) {
+            try {
+                configFile.delete();
+            } catch (Exception e) {
+                LOGGER.warn("Failed to delete empty config file: ", e);
+            }
+        }
+
         HANDLER.load();
-        TooltipRegistry.reload();
+
+        if (isNewOrEmpty) save();
+        else TooltipRegistry.reload();
     }
 
     public static TooltipConfig getConfig() {
